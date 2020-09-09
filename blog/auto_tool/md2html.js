f571bfcb4952ec757fa30f84cc30303b380d83fd
root <root@silviaseDESTKTOP.localdomain>
2020-09-09 18:51:55 +0900

    add new article

--- blog/auto_tool/md2html.js
+++ blog/auto_tool/md2html.js
@@ -1,17 +1,14 @@
-const program = require("commander");
-const fs = require("fs");
-const marked = require("marked");
+const program = require('commander');
+const fs = require('fs');
+const marked = require('marked');
 
 program.parse(process.argv);
-const path = program.args[0];
 
-fs.readFile(path, {encoding: "utf8"}, (err, file) => {
-    if (err) {
-        console.error(err.message);
-        process.exit(1);
-        return;
-    }
+const path = require('path');
+const html_head = fs.readFileSync(path.resolve(__dirname, 'head.txt'), 'utf8');
+const html_body = marked(fs.readFileSync(program.args[0], 'utf8'));
+const html_foot = fs.readFileSync(path.resolve(__dirname, 'foot.txt'), 'utf8');
 
-    const html = marked(file);
-    console.log(html);
-});
+fs.writeFileSync(program.args[1], html_head);
+fs.appendFileSync(program.args[1], html_body);
+fs.appendFileSync(program.args[1], html_foot);
